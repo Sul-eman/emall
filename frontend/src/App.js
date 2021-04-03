5a909988d1c6a594766ff8926fb90b9e01210c50
Suleman Saleem <msulemanhere@gmail.com>
2021-04-03 01:30:14 -0500

    set up payment method screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,25 +12,27 @@ import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
 import ShippingScreen from "./screens/ShippingScreen";
+import PaymentScreen from "./screens/PaymentScreen";
 
 function App() {
-	return (
-		<Router>
-			<Header />
-			<main className="py-3">
-				<Container>
-					<Route path="/" component={HomeScreen} exact />
-					<Route path="/products/:id" component={ProductScreen} />
-					<Route path="/cart/:id?" component={CartScreen} />
-					<Route path="/login" component={LoginScreen} />
-					<Route path="/register" component={RegisterScreen} />
-					<Route path="/profile" component={ProfileScreen} />
-					<Route path="/shipping" component={ShippingScreen} />
-				</Container>
-			</main>
-			<Footer />
-		</Router>
-	);
+  return (
+    <Router>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Route path="/" component={HomeScreen} exact />
+          <Route path="/products/:id" component={ProductScreen} />
+          <Route path="/cart/:id?" component={CartScreen} />
+          <Route path="/login" component={LoginScreen} />
+          <Route path="/register" component={RegisterScreen} />
+          <Route path="/profile" component={ProfileScreen} />
+          <Route path="/shipping" component={ShippingScreen} />
+          <Route path="/payment" component={PaymentScreen} />
+        </Container>
+      </main>
+      <Footer />
+    </Router>
+  );
 }
 
 export default App;
